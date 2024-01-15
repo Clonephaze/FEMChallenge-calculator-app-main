953d57fb939c5f81e72af6606c896c186e1a0674
Clonephaze <jackdsmith2327@gmail.com>
2024-01-15 01:26:06 -0800

    Challenge Complete

---
+++ calculator.js
@@ -0,0 +1,71 @@
+let display = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let mathCollection = '';
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (display.getAttribute('data-answered') == 'true') {
+            if (e.target.innerText == '=' || e.target.innerText == '+' || e.target.innerText == '-' || e.target.innerText == 'x' || e.target.innerText == '/') {
+                return
+            } else if (e.target.innerText == 'RESET' || e.target.innerText == 'DEL') {
+                display.innerText = '0';
+                display.setAttribute('data-answered', 'false');
+            } else {
+                display.innerText = e.target.innerText;
+                display.setAttribute('data-answered', 'false');
+            }
+        } else {
+            switch(e.target.innerText){
+                case 'RESET':
+                    display.innerText = '0';
+                    mathCollection = '';
+                break;
+                case 'DEL':
+                    if(display.innerText.length == 1){
+                        display.innerText = '0';
+                    } else {
+                        display.innerText = display.innerText.slice(0, -1);
+                    }
+                break;
+                case '+':
+                case '-':
+                case 'x':
+                case '/':
+                    mathCollection += display.innerText + e.target.innerText;
+                    console.log(mathCollection);
+                    display.innerText = '0';
+                break;
+                case '=':
+                    if (mathCollection.length == 0) {
+                        return
+                    } else {
+                        try {
+                            let answer = eval(mathCollection + display.innerText);
+                            if (typeof answer === 'number') {
+                                display.innerText = answer;
+                                mathCollection = '';
+                                display.setAttribute('data-answered', 'true');
+                            } else {
+                                display.innerText = 'ERROR';
+                                mathCollection = '';
+                                display.setAttribute('data-answered', 'true');
+                            }
+                        } catch (error) {
+                            display.innerText = 'ERROR';
+                            mathCollection = '';
+                            display.setAttribute('data-answered', 'true');
+                        }
+                    }
+                break;
+                default:
+                    if(display.innerText === '0'){
+                        display.innerText = '';
+                        display.innerText += e.target.innerText;
+                    } else {
+                        display.innerText += e.target.innerText;
+                    }
+                break;
+            }
+        }
+    })
+})
